0f5ccb6c0c600fbec01f6c89a6f98eca38294774
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-11-16 18:34:33 +0100

    create test.py

---
+++ Python/lecon/tkinter/test.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+mainWin = Tk("screenName: Optional[str]=...", "baseName: Optional[str]=...")
+
+text1 = Label(mainWin, text="je suis en vie")
+text1.pack()
+
+mainWin.mainloop()
