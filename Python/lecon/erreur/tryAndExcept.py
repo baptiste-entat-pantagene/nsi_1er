b77479c896d402e4a4a9703780f585b4568f1c32
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-11-16 14:19:34 +0100

    Update tryAndExcept.py

---
+++ Python/lecon/erreur/tryAndExcept.py
@@ -0,0 +1,22 @@
+"""La structure try et except"""
+
+
+try:
+    age = int(input("votre age ?\n")) #le programme demande l'age et le stucke dans la variable -->age
+except: #si l'utilisateur utilise par exemple des lettre, la conversion en ->int<- de ->age<- par int() vas cree une erreur et ->execpt<- sera executer
+    print("vous devez n'utiliser que des nombres...")
+
+print("vous avez donc {age} ans !".format(age = age)) #si tout vas bien le print s'execute.
+
+
+"""version dans une boucle"""
+
+while True: #avec True la boucle tourne sans jamais s'arreter
+    try:
+        age = int(input("votre age ?\n"))
+        break #permet de sortir de la boucle avant de la finir
+    except:
+        print("vous devez n'utiliser que des nombres...")
+        continue #permet de recomencer au debut de la boucle
+
+print("vous avez donc {age} ans !".format(age = age))
