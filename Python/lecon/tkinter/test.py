4bb0954fe9dfd19f55f1504fde68cfc81538d2ca
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-11-16 18:58:09 +0100

    Update test.py

--- Python/lecon/tkinter/test.py
+++ Python/lecon/tkinter/test.py
@@ -5,8 +5,6 @@ mainWin = Tk()
 def work():
     print("nice work")
 
-text1 = Label(mainWin, text="je suis en vie")
-text1.pack()
 but1 = Button(mainWin, text="clique !", command=work)
 but1.pack()
 
