dab14ee4b492bba7c66a1160a79e54f8e670b0a5
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-11-16 14:32:53 +0100

    Create boucleFor.py

---
+++ Python/lecon/base/boucleFor.py
@@ -0,0 +1,15 @@
+"""boucleFor"""
+"""La boucle for peut prendre range() en argument,
+    range() luis prend en argument -> un point de depart (souvent 0), un pooint de fin (3 dans l'exemple 1) et un pas (si non mentionner =1 -> exemple 1)
+    La boucle For dois aussi avoir une variable pour itterer (compter) elle est accesible librement en python"""
+
+print("\nexemle 1:")
+for a in range(0, 3): #For s'arrete avant d'atteindre 3, donc a 2
+    print(a)
+
+
+print("\nexemle 2:")
+for b in range(3, 0, -1): # il est possible d'iterer a l'enver
+    print(b)
+
+#il n'est passible d'utiliser de nombre flotant avec For
