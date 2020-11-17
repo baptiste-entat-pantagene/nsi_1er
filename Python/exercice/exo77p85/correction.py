3663aa1a2d0a8a105091c8cb86483b9e4779fa0a
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-11-17 20:34:48 +0100

    Update correction.py

--- Python/exercice/exo77p85/correction.py
+++ Python/exercice/exo77p85/correction.py
@@ -7,10 +7,3 @@ tab2 = [10, 20, 30]
 
 tabAdd = concatenation(tab1, tab2)
 print(tabAdd)
-
-
-listeX = [[0]*5]*2 #créer un tableau de 2 lignes, chacune remplie avec 5 zéros
-listeY = [[0]*5,[0]*5] #Résultat identique qu'à la ligne du dessus
-
-listeX[0][0] = 6 #Mets la valeur 6 dans la case du tableau, première ligne et première colonne
-listeY[0][0] = 6 #Résultat identique qu'à la ligne du dessus
