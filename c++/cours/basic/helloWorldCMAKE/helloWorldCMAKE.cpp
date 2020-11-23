68771386d4aaa34b55919acc1c359962e981d6f9
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-11-23 17:58:52 +0100

    cmake project

---
+++ c++/cours/basic/helloWorldCMAKE/helloWorldCMAKE.cpp
@@ -0,0 +1,12 @@
+﻿// helloWorldCMAKE.cpp : définit le point d'entrée de l'application.
+//
+
+#include "helloWorldCMAKE.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
