c5236bddfaf08b87827b270d5feee20de738365c
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-11-23 22:13:33 +0100

    create vector and array section

---
+++ c++/cours/conteneurs/array/array.cpp
@@ -0,0 +1,17 @@
+﻿// array.cpp : définit le point d'entrée de l'application.
+//
+
+#include "array.h"
+
+using namespace std;
+
+int main()
+{
+    array<int, 5> const tableau_entiers{ 1, 4, 2, 4, 7 };
+
+    cout << "Je vais afficher mon tableau de " << tableau_entiers.size() << " entier.\n";
+    for (auto const element : tableau_entiers)
+    {
+        cout << element << endl;
+    }
+}
---
+++ c++/cours/conteneurs/vector/vector.cpp
@@ -0,0 +1,17 @@
+﻿// vector.cpp : définit le point d'entrée de l'application.
+//
+
+#include "vector.h"
+
+using namespace std;
+
+int main()
+{
+    vector<int> const tableau_entiers{ 1, 2, 3 };
+
+    cout << "Je vais afficher mon tableau de " << tableau_entiers.size() << " entier.\n";
+    for (auto const element : tableau_entiers)
+    {
+        cout << element << endl;
+    }
+}
