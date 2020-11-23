c9427642f91a27d270c0861655fa359f55268074
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-11-23 18:32:20 +0100

    input c++ section

---
+++ c++/cours/basic/input/input.cpp
@@ -0,0 +1,27 @@
+﻿// input.cpp : définit le point d'entrée de l'application.
+//
+
+#include "input.h"
+
+using namespace std;
+
+int main()
+{
+	string buffer;
+	int age;
+
+	cout << "tape ton age\n";
+
+	cin >> buffer;
+	try
+	{
+		age = stoi(buffer);
+	}
+	catch (const std::exception&)
+	{
+		cout << "error de toi/n/n";
+		return -1;
+	}
+	cout << "vous avez taper : " << age << "ans\nworkNice\n";
+	return 0;
+}
