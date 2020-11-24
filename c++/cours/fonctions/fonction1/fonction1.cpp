502fd6eef41c557adf213421d59ec21b489b6d19
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-11-24 14:27:54 +0100

    create c++ fonction section

---
+++ c++/cours/fonctions/fonction1/fonction1.cpp
@@ -0,0 +1,20 @@
+﻿// fonction1.cpp : définit le point d'entrée de l'application.
+//
+
+#include "fonction1.h"
+
+using namespace std;
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int main()
+{
+	int a, b, c;
+	a = 5;
+	b = 8;
+	c = add(a, b);
+	cout << "a + b = " << c << endl;
+	return 0;
+}
