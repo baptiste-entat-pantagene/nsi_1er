f628c957b9ea970c8a6592f92bd1c9b13d8d7587
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-11-25 16:47:36 +0100

    update

---
+++ c++/bricBracFolder/bricBrac/bricBrac.cpp
@@ -0,0 +1,22 @@
+﻿// bricBrac.cpp : définit le point d'entrée de l'application.
+//
+
+#include "bricBrac.h"
+
+using namespace std;
+
+int main()
+{
+    ofstream flux("text.txt");  // on ouvre le flux en lecture
+
+    if (flux)  // si l'ouverture a réussi
+    {
+        flux << "salut";
+        flux.close();  // on ferme le flux
+    }
+    else  // sinon
+        cerr << "Impossible d'ouvrir le fichier !" << endl;
+
+
+	return 0;
+}
