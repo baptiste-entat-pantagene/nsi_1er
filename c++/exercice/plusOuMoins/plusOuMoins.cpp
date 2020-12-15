dc34709e3a37c781dc29399c370f2223214a4b62
Baptiste Entat <bapt84@gmail.com>
2020-12-15 11:39:49 +0100

    update

--- c++/exercice/plusOuMoins/plusOuMoins.cpp
+++ c++/exercice/plusOuMoins/plusOuMoins.cpp
@@ -1,15 +1,30 @@
 // plusOuMoins.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 #include <iostream>
-#include <random>
-#include <ctime>
+/* rand example: guess the number */
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
 using namespace std;
 
 
 int main()
 {
-    auto const seed = 5; // génération de la graine
-    std::default_random_engine engin{ seed }; // générateur aléatoire
-    std::normal_distribution<float> normal(0, 1); // générateur de distribution
-    std::cout << normal(engin) << std::endl; // génération
+    int randNum, iGuess;
+
+    /* initialize random seed: */
+    srand(time(NULL));
+    /* generate secret number between 1 and 10: */
+    randNum = rand() % 100 + 1;
+
+    do {
+        cout << "Guess the number (1 to 100): \n";
+        cin >> iGuess;
+        if (randNum < iGuess) cout << "The secret number is lower\n";
+        else if (randNum > iGuess) cout << "The secret number is higher\n";
+    } while (randNum != iGuess);
+
+    cout << "Congratulations!";
+    return 0;
 }
