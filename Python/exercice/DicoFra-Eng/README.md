5d2ea2da1bcc7b66179121e59504bd153d2336f8
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-12-18 23:26:41 +0100

    new exercice dico Fr-Ang

---
+++ Python/exercice/DicoFra-Eng/README.md
@@ -0,0 +1,17 @@
+# dictionnaire français – anglais
+
+On souhaite créer un dictionnaire français – anglais. Pour cela, nous allons créer le dictionnaire "traduction", dont chaque clé sera constitué d'un mot en français, et chaque valeur sera constituée de sa traduction en anglais.
+
+**Copiez après chaque question le code Python permettant de répondre aux questions suivantes.** Testez le programme dans son ensemble
+
+1. Choisissez 5 mots de la langue française, et créez le dictionnaire traduction.
+
+2. Ajoutez une entrée au dictionnaire de la question précédente.
+
+3. Écrivez une fonction anglais qui affiche, ligne par ligne, tous les mots en anglais du dictionnaire.
+
+4. Écrivez une fonction francais qui affiche, ligne par ligne, tous les mots en français du dictionnaire.
+
+5. Écrivez une fonction ajoute prenant mot1, mot2 et dico comme arguments (mot1 représente le mot en français, mot2 représente le mot en anglais, et dico représente le dictionnaire à modifier), et ajoute cet ensemble dans le dictionnaire uniquement si la clé n'est pas présente dans le dictionnaire.
+
+6.  **(Bonus)** Écrivez une fonction supprime prenant lett et dico comme arguments et qui supprime du dictionnaire toutes les entrées dont la clé commence par la lettre lett.
---
+++ Python/exercice/DicoFra-Eng/solution.py
@@ -0,0 +1,49 @@
+"""
+Baptiste Entat 18 Dec 2020
+https://github.com/bapt800/nsi_1er/tree/main/Python/exercice/DicoFra-Eng
+"""
+
+#1. Choisissez 5 mots de la langue française, et créez le dictionnaire traduction.
+
+traduction = {"ordinateur": "computer", "ordinateur portable": "laptop", "clavier": "keyboard", "souris": "mouse", "écran": "screen"}
+
+
+#2. Ajoutez une entrée au dictionnaire de la question précédente.
+traduction["pomme"] = "apple" #(pomme est une bonne marque d'informatique)
+
+
+#3. Écrivez une fonction anglais qui affiche, ligne par ligne, tous les mots en anglais du dictionnaire.
+def anglais(dictIn:dict):
+    for i in dictIn.values():
+        print("anglais -->", i)
+anglais(traduction)
+
+
+#4. Écrivez une fonction francais qui affiche, ligne par ligne, tous les mots en français du dictionnaire.
+def francais(dictIn:dict):
+    for i in dictIn.keys():
+        print("français -->", i)
+francais(traduction)
+
+
+#5. Écrivez une fonction ajoute prenant "mot1", "mot2" et "dico" comme arguments (mot1 représente le mot en français, mot2 représente le mot en anglais, 
+# et "dico" représente le dictionnaire à modifier), 
+# et ajoute cet ensemble dans le dictionnaire uniquement si la clé n'est pas présente dans le dictionnaire.
+def ajoute(motFr:str, motAng:str, dictIn:dict):
+    if motFr is dictIn:
+        pass
+    else:
+        dictIn[motFr] = motAng
+    return dictIn
+print(ajoute("garçon de jeu", "Game-Boy", traduction))
+
+
+#BONUS
+#6. Écrivez une fonction "supprime" prenant "lett" et "dico" comme arguments et qui supprime du dictionnaire toutes les entrées dont la clé commence par la lettre lett.
+def supp(lett:str, dictIn:dict):
+    dictOut:dict = {}
+    for k, v in dictIn.items():
+        if k[0] != lett:
+            dictOut[k] = v
+    return dictOut
+print(supp('g', traduction))
