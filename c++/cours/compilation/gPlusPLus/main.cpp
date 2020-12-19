60d51146578d3c037740c4c61bdc2caa724f3c7d
bapt800 <32511746+bapt800@users.noreply.github.com>
2020-12-19 18:51:14 +0100

    clean c++ section

---
+++ c++/cours/compilation/gPlusPLus/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    for (int i = 0; i < 10000; i++)
+    {
+        std::cout<< i;
+    }
+    std::cout<< endl;
+    system("pause");
+}
--- c++/cours/conteneurs/vector/vector.h
+++
@@ -1,9 +0,0 @@
-﻿// vector.h : fichier Include pour les fichiers Include système standard,
-// ou les fichiers Include spécifiques aux projets.
-
-#pragma once
-
-#include <iostream>
-#include <vector>
-
-// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
