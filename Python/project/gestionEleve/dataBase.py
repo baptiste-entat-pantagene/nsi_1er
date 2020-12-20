ed708ae6055f78eb02e714174c75c2167bea1a6f
Baptiste Entat <bapt84@gmail.com>
2020-12-20 13:41:37 +0100

    upadate dataBase.py

--- Python/project/gestionEleve/dataBase.py
+++ Python/project/gestionEleve/dataBase.py
@@ -1,4 +1,3 @@
-from os import getlogin
 
 
 class DataBase:
@@ -6,47 +5,68 @@ class DataBase:
         
         self.FLuxdata = ""
         self.data = ""
+        self.itt = 0
+        self.ligneWork = ""
+        self.ligneKey = ""
 
+        self.dictData = {}
         self.listEleve = []
         self.listProf = []
+        self.listNote = []
     
     def load(self, cheminFichier:str):
         self.Fluxdata = open(cheminFichier, 'r')
-        data = self.Fluxdata.read()
-        print("*->", data)
-        itt = 0
-        ligneWork = ""
-        ligneKey = ""
-        while ligneKey != "$exit":
-            print("curent char ->", data[itt])
-            print("ln ->", ligneKey)
-            if data[itt] == '$':
-                ligneKey = ""
-                while data[itt] != '%':
-                    print("curent char ->", data[itt])
-                    print("ln ->", ligneKey)
-                    ligneKey += data[itt]
-                    itt +=1
-            while data[itt] != '$':
-                if ligneKey == "$eleve":
-                    ligneWork = ""
-                    itt += 1
-                    while data[itt] != '\n' and data[itt] != '$':
-                        print("curent char ->", data[itt])
-                        print("ln ->", ligneWork)
-                        ligneWork += data[itt]
-                        itt +=1
-                    if ligneWork != '':
-                        self.listEleve.append(ligneWork)
-                elif ligneKey == "$prof":
-                    ligneWork = ""
-                    itt += 1
-                    while data[itt] != '\n' and data[itt] != '$':
-                        print("curent char ->", data[itt])
-                        print("ln ->", ligneWork)
-                        ligneWork += data[itt]
-                        itt +=1
-                    if ligneWork != '':
-                        self.listProf.append(ligneWork)
-                elif ligneKey == "$exit":
+        self.data = self.Fluxdata.read()
+        print("*->", self.data)
+        self.itt = 0
+        self.ligneWork = ""
+        self.ligneKey = ""
+        while self.ligneKey != "$exit": #boucle principale
+            print("curent char ->", self.data[self.itt])
+            print("ln ->", self.ligneKey)
+            if self.data[self.itt] == '$': #cherche une clee
+                self.ligneKey = ""
+                while self.data[self.itt] != '%':
+                    print("curent char ->", self.data[self.itt])
+                    print("ln ->", self.ligneKey)
+                    self.ligneKey += self.data[self.itt]
+                    self.itt +=1
+
+            while self.data[self.itt] != '$': #cherche une donnee
+                if self.ligneKey == "$eleve": #pour $eleve
+                    self.ReadLigne(self.listEleve) 
+                elif self.ligneKey == "$prof": #pour $prof
+                    self.ReadLigne(self.listProf) 
+#                elif self.ligneKey == "$note": #pour $note
+#                    ligneKeyNd = ""
+#                    self.ReadLigne()
+#                    self.ReadLigne()
+#                    if self.ligneWork[0] == '&':
+#                        ligneKeyNd = self.ligneWork
+#
+#                    while self.ligneWork != '%':
+#                        self.ReadLigne()
+
+
+
+
+                elif self.ligneKey == "$exit": #si la clee est "$exit" -> sortie du programme
                     break
+                else: #gestion de fichier mort
+                    print("!!-> erreur dans la base de donnee --> '", cheminFichier, "' <-!!")
+                    self.ligneKey = "$exit"
+                    break
+
+    def ReadLigne(self, listX = None):
+        self.ligneWork = ""
+        self.itt += 1
+        while self.data[self.itt] != '\n' and self.data[self.itt] != '$':
+            print("curent char ->", self.data[self.itt])
+            print("ln ->", self.ligneWork)
+            self.ligneWork += self.data[self.itt]
+            self.itt +=1
+        if listX != None and self.ligneWork != '':
+            listX.append(self.ligneWork)
+        if listX == None and self.ligneWork != '':
+            pass #return not implemented
+    
