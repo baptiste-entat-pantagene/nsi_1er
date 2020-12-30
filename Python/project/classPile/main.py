22769142d0f8b1f05ae6eb29fa8924b6d265e566
Baptiste Entat <bapt84@gmail.com>
2020-12-30 18:11:44 +0100

    Update main.py

--- Python/project/classPile/main.py
+++ Python/project/classPile/main.py
@@ -13,13 +13,17 @@ from classPile import *
 print("\n--->> start main.py <<---")
 #main code
 
-listeDep = (1, 5, 2, 3)
-
-pile = ClassPile(sizePile=4, listIn=None, debugLevel= 1) #creation de la pile
+dictDepartement = {84: "Vaucluse", 4: "Alpes-de-Haute-Provence", 6: "Alpes-Maritimes", 5: "Hautes-Alpes",  13: "Bouches-du-Rhône", 83: "Var"}
 
 
-pile.pushList(listeDep)
+pile = ClassPile(sizePile=None, listIn=list(dictDepartement.keys()), debugLevel= 1) #creation de la pile
+
+pile.fx_sort() #ordre croisant
+pile.fx_reverse() #-> passer en ordre decroisant
+
+for i in range(0, len(dictDepartement)):
+    print(pile.get_last(), "->", dictDepartement[pile.get_lastAndPop()])
+
+
 pile.debug_statPile()
-
-
 print("--->> exit main.py <<---\n")
