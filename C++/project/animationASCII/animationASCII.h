c893d071520a47acc228b2e44d5de7333161d59c
Baptiste Entat <bapt84@gmail.com>
2021-01-15 14:30:12 +0100

    update bg

--- C++/project/animationASCII/animationASCII.h
+++ C++/project/animationASCII/animationASCII.h
@@ -5,6 +5,13 @@
 
 #include <iostream>
 #include <string>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
 
 // TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
 #include "Render.h"
+
+
+void addToRender(std::string  render[10][10], std::string  obj[4][3], int x, int y);
+void clearRender(std::string  render[10][10]);
+void extractRender(std::string& buff, std::string  render[10][10]);
