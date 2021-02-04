21749b11db86c63a0c63a6f74d5f371a302a80ca
Baptiste Entat <bapt84@gmail.com>
2021-02-04 20:23:38 +0100

    Poiteur --> Gestion d'une mémoire insuffisante

---
+++ C++/cours/pointeur/GestionMémoireInsuffisante/GestionMémoireInsuffisante.cpp
@@ -0,0 +1,20 @@
+﻿// GestionMémoireInsuffisante.cpp : définit le point d'entrée de l'application.
+//
+
+#include "GestionMémoireInsuffisante.h"
+
+using namespace std;
+constexpr auto BIG_NUMBER = 100000000;
+
+int main()
+{
+    int* pI = new int[BIG_NUMBER];
+    if (pI == 0x0) {
+        cout << "Insufficient memory" << endl;
+        return -1;
+    }
+    cout << "\nnice";
+    
+    delete[] pI;
+    
+}
