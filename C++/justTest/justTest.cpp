deb860d49a14a8f00077379dd5d8ca910c8333e0
Baptiste Entat <bapt84@gmail.com>
2021-02-09 19:09:41 +0100

    juste create a test

---
+++ C++/justTest/justTest.cpp
@@ -0,0 +1,17 @@
+﻿// justTest.cpp : définit le point d'entrée de l'application.
+//
+
+#include "justTest.h"
+
+using namespace std;
+
+int main()
+{
+	int myVar = 5;
+	int* pointeToIntVar = &myVar;
+	
+	cout << &myVar << endl;
+	cout << pointeToIntVar << endl;
+	cout << *pointeToIntVar << endl;
+	return 0;
+}
