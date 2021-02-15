8cdb92423658099ec05aaecaeae98afd5ca1e3ad
Baptiste Entat <bapt84@gmail.com>
2021-02-15 15:25:04 +0100

    update helloQt5

--- C++/Qt5/helloQt5/widget.cpp
+++ C++/Qt5/helloQt5/widget.cpp
@@ -6,6 +6,7 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    ui->label->hide(); //cache le "salut"
 }
 
 Widget::~Widget()
@@ -13,3 +14,10 @@ Widget::~Widget()
     delete ui;
 }
 
+
+void Widget::on_pushButton_clicked()
+{
+    ui->pushButton->hide();
+    ui->label->show();
+
+}
