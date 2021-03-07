4cae4186755cd28c52b1ea806f2f65c1be682135
Baptiste Entat <bapt84@gmail.com>
2021-03-07 19:25:37 +0100

    add comment and more pretty user ui

--- Python/project/Mini-project1-V2/window.py
+++ Python/project/Mini-project1-V2/window.py
@@ -6,6 +6,7 @@ import tkinter as tk
 class Application(tk.Tk):
     def __init__(self, dataMethod) -> None:
         tk.Tk.__init__(self)
+        self.dataMethod = dataMethod
         self.gateway = Gateway.Gateway(requestMethod=dataMethod)
 
         self.selectFrame = tk.Frame(self)
@@ -54,9 +55,17 @@ class Application(tk.Tk):
         #productName = tk.StringVar().set(buffvar)
         tk.Label(self.infoFrame, text=buffvar).grid(row=0, column=0)
 
-        optionInside = ("name", "ingredients", "code", "ecoscore_score",
-                        "ecoscore_grade", "nutriscore_grade", "stores",
-                        "packaging", "quantity", "brands", "labels")
+        optionInside = ()
+        if self.dataMethod == 0:
+            optionInside = ("name", "ingredients", "code", "ecoscore_score",
+                            "ecoscore_grade", "nutriscore_grade", "stores",
+                            "packaging", "quantity", "brands", "labels")
+        elif self.dataMethod == 1:
+            optionInside = ("name", "ingredients", "code")
+
+        else:
+            raise NotImplementedError("optionInside != 0 and 1 ")
+
         for i in range(len(optionInside)):
             try:
                 msgBuff: str = str(
@@ -66,4 +75,8 @@ class Application(tk.Tk):
             except:
                 msgBuff: str = str(
                     optionInside[i]) + " --> " + "information inconnu"
+
             tk.Label(text=msgBuff).grid(row=(i + 2), column=0)
+        
+        if self.dataMethod == 1:
+            tk.Label(text="(if you want more facts, please use online request method)").grid(row=6, column=0)
