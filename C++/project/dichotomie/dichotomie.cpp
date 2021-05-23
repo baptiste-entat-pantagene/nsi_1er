7f29b49badc343d74666650ca6c485b120c34274
Baptiste Entat <bapt84@gmail.com>
2021-05-23 13:31:44 +0200

    create dichotomie project

---
+++ C++/project/dichotomie/dichotomie.cpp
@@ -0,0 +1,12 @@
+﻿// dichotomie.cpp : définit le point d'entrée de l'application.
+//
+
+#include "dichotomie.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "it's work !" << endl;
+	return 0;
+}
